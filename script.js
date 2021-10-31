4e71da4723fddc8a93b2ff7f149279a09b9715fc
ManuelYosia <manuelyosia44@gmail.com>
2021-10-31 22:06:21 +0700

    menambahkan konten 1

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 // ham menu
 const hamBtn = document.getElementsByClassName('menu-toggle') [0];
 const menuSelect = document.querySelector('nav ul');
+
 hamBtn.addEventListener("click", () => {
     hamBtn.classList.toggle('active')
     menuSelect.classList.toggle('slide');
 });
+
 const nav = document.getElementsByTagName('nav')[0]
 const a = nav.getElementsByTagName('a');
+
 for (let i in a) {
     a[i].addEventListener('click', () => {
         hamBtn.classList.remove('active');
